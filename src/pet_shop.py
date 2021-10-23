0c9cd5e0e2c643a4e6020d54eec34d42dc73f9fb
Neddon13 <91260623+Neddon13@users.noreply.github.com>
2021-10-23 16:18:12 +0100

    third commit

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -19,11 +19,39 @@ def get_stock_count(pet_shop_name):
     return len(pet_shop_name["pets"])
 
 def get_pets_by_breed(pet_shop_name, breed):
-    found_pets = []
+    pets_found = []
     for pet in pet_shop_name["pets"]:
         if pet["breed"] == breed:
-            found_pets.append(pet)
-    return found_pets
+            pets_found.append(pet)
+    return pets_found
 
+def get_pets_by_breed(pet_shop_name, breed):
+    pets_not_found = []
+    for pet in pet_shop_name["pets"]:
+        if pet["breed"] == breed:
+            pets_not_found.append(pet)
+    return pets_not_found
 
+def find_pet_by_name(pet_shop_name, pet_name):
+    for pet in pet_shop_name["pets"]:
+        if pet["name"] == pet_name:
+            return pet
+    
+def remove_pet_by_name(pet_shop_name, pet_name):
+    delete_pet = find_pet_by_name(pet_shop_name, pet_name)
+    pet_shop_name["pets"].remove(delete_pet)
 
+def add_pet_to_stock(pet_shop_name, new_pet):
+    pet_shop_name["pets"].append(new_pet)
+
+def get_customer_cash(pet_shop_name):
+    return pet_shop_name["cash"]
+
+def remove_customer_cash(customer_name, amount):
+    customer_name["cash"] -= amount 
+    
+def get_customer_pet_count(pet_shop_name):
+    return len(pet_shop_name['pets'])
+
+def add_pet_to_customer(customer_name, pet):
+    customer_name['pets'].append(pet)
